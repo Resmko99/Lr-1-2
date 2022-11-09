3e83d5e9eff238e4fddeaea7fdd1045c699955d0
Resmko99 <117806961+Resmko99@users.noreply.github.com>
2022-11-09 10:25:18 +0300

    Add files via upload

---
+++ 27 zad.pas
@@ -0,0 +1,19 @@
+﻿var a,b,c,d,e,mx,sr,mn:integer;
+begin
+read(a,b,c);
+if (a+b<=c)or(a+c<=b)or(b+c<=a)
+then writeln('impossible')
+else
+ begin
+  if a>b then mx:=a else mx:=b;
+  if c>mx then mx:=c;
+  if a<b then mn:=a else mn:=b;
+  if c<mn then mn:=c;
+  sr:=a+b+c-mx-mn;
+  d:=mn*mn+sr*sr;
+  e:=mx*mx;
+  if d>e then writeln('acute')
+  else if d<e then writeln('obtuse')
+  else if d=e then writeln('right');
+ end;
+end.
